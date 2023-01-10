9fd9d53f9c5e8460510e2304a143ade6b3e0072d
chengmingpei <chengmingpei@pingduoduo.com>
2023-01-11 01:00:07 +0800

    feat(singleTask): add singleTask test

--- app/src/main/java/com/cain/androidpro/event/EventActivity.java
+++ app/src/main/java/com/cain/androidpro/event/EventActivity.java
@@ -2,12 +2,15 @@ package com.cain.androidpro.event;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.cain.androidpro.MainActivity;
 import com.cain.androidpro.R;
+import com.cain.androidpro.net.OkHttpActivity;
 
 public class EventActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
     public static final String TAG = "EventDemo";
@@ -20,6 +23,7 @@ public class EventActivity extends AppCompatActivity implements View.OnClickList
         findViewById(R.id.btv_event_demo).setOnTouchListener(this);
         findViewById(R.id.tv_event_demo).setOnTouchListener(this);
         findViewById(R.id.tv_event_demo).setOnClickListener(this);
+        findViewById(R.id.btv_start_main).setOnClickListener(this);
         //        findViewById(R.id.cl_root).setOnTouchListener(this);
         //        findViewById(R.id.cl_root).setOnClickListener(this);
     }
@@ -29,6 +33,9 @@ public class EventActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()) {
             case R.id.btv_event_demo:
                 Log.d(TAG, "EventActivity#onClick: event demo btn is clicked.");
+                break;
+            case R.id.btv_start_main:
+                startActivity(new Intent(this, MainActivity.class));
                 break;
             case R.id.cl_root:
                 Log.d(TAG, "EventActivity#onClick: event demo cl root is clicked.");
